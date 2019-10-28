e0ed78bc964ee764e27b0b7aaeebd38d39bb54f3
Radoslav <r.genchew@gmail.com>
2019-10-28 21:23:37 +0100

    Modified gatsby-config.js file by changing the title in 'siteMetadata'

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   pathPrefix: `/gatsby-starter-hyperspace/`, // This path is subpath of your hosting https://domain/portfolio
   siteMetadata: {
-    title: 'Gatsby Starter Hyperspace',
+    title: 'Posithing',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Hyperspace',
+        name: 'Posithing',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
