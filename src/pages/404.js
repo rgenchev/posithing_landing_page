97241141d959153579197347840ec6bb1f797f32
Radoslav <r.genchew@gmail.com>
2019-11-10 18:25:27 +0100

    Modified 404 to redirect to root

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../components/layout';
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>Not a valid URL</p>
-  </Layout>
-);
+import { Link } from "gatsby"
+
+import { navigate } from 'gatsby'
+
+const NotFoundPage = () => {
+  navigate('/');
+
+  return null;
+}
 
 export default NotFoundPage;
