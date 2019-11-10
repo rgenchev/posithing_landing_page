f68656a3090aee8e20deaf43a1c2d63abf2b1252
Radoslav <r.genchew@gmail.com>
2019-11-10 18:26:49 +0100

    Index page: some minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import Footer from '../components/Footer';
@@ -11,7 +10,7 @@ const IndexPage = () => (
     <Sidebar />
 
     <div id="wrapper">
-      <section id="intro" className="wrapper style1 fullscreen fade-up">
+      <section id="welcome" className="wrapper style1 fullscreen fade-up">
         <div className="inner">
           <h1>Posithing</h1>
           <p>
@@ -20,8 +19,8 @@ const IndexPage = () => (
           </p>
           <ul className="actions">
             <li>
-              <Scroll type="id" element="one">
-                <a href="#one" className="button">
+              <Scroll type="id" element="about">
+                <a href="#about" className="button" style={{backgroundColor: '#C6008F'}}>
                   Научи повече
                 </a>
               </Scroll>
@@ -30,7 +29,7 @@ const IndexPage = () => (
         </div>
       </section>
 
-      <section id="one" className="wrapper style2 spotlights">
+      <section id="about" className="wrapper style2 spotlights">
         <section>
           <div className="content">
             <h2>За нас</h2>
@@ -51,7 +50,7 @@ const IndexPage = () => (
         
       </section>
 
-      <section id="two" className="wrapper style3 fade-up">
+      <section id="realization" className="wrapper style3 fade-up">
         <div className="inner">
           <h2>Реализация на проекта</h2>
           <p>
@@ -110,7 +109,7 @@ const IndexPage = () => (
         </div>
       </section>
 
-      <section id="three" className="wrapper style1 fade-up">
+      <section id="join" className="wrapper style1 fade-up">
         <div className="inner">
           <h2>Присъедини се</h2>
           <p>
@@ -118,7 +117,7 @@ const IndexPage = () => (
           </p>
           <div className="split style1">
             <section>
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLScpdfpaERU2SHD62s2qyqOCBUb9MULQ3D6LdcLMzqMGpbdDJA/viewform?usp=sf_link" className="button" target="_blank" style={{backgroundColor: '#C6008F'}}>
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLScpdfpaERU2SHD62s2qyqOCBUb9MULQ3D6LdcLMzqMGpbdDJA/viewform?usp=sf_link" className="button" target="_blank" rel="noopener noreferrer" style={{backgroundColor: '#C6008F'}}>
                 Да действаме!
               </a>
             </section>
@@ -126,19 +125,19 @@ const IndexPage = () => (
               <ul className="contact">
                 <li>
                   <h3>За повече информация</h3>
-                  <a href="/#">r.genchew@gmail.com</a><br/>
-                  <a href="/#">maykalgenchev@gmail.com</a>
+                  <a href="mailto:r.genchew@gmail.com">r.genchew@gmail.com</a><br/>
+                  <a href="mailto:maykalgenchev@gmail.com">maykalgenchev@gmail.com</a>
                 </li>
                 <li>
                   <h3>Социални медии</h3>
                   <ul className="icons">
                     <li>
-                      <a href="/#" className="fa-facebook">
+                      <a href="https://www.facebook.com/posithing" className="fa-facebook" target="_blank" rel="noopener noreferrer">
                         <span className="label">Facebook</span>
                       </a>
                     </li>
                     <li>
-                      <a href="/#" className="fa-github">
+                      <a href="https://github.com/rgenchev/posithing_landing_page" className="fa-github" target="_blank" rel="noopener noreferrer">
                         <span className="label">GitHub</span>
                       </a>
                     </li>
