f59c9004c90a287342f799c9b6d8bfaf17da273b
Radoslav <r.genchew@gmail.com>
2019-11-10 18:28:43 +0100

    404 page: revert changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,12 @@ import Layout from '../components/layout';
 
 import { Link } from "gatsby"
 
-import { navigate } from 'gatsby'
+const NotFoundPage = () => (
+  <Layout>
+    <h3>За съжаление страницата, която търсиш, не беше открита :(</h3>
 
-const NotFoundPage = () => {
-  navigate('/');
-
-  return null;
-}
+    <Link to="/" className="button" style={{backgroundColor: '#C6008F'}}>Върни се обратно на главната страница!</Link>
+  </Layout>
+);
 
 export default NotFoundPage;
