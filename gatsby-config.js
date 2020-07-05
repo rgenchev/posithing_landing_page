205ce52eef68baa56fd6be780848d77c0dd40bbf
Radoslav Genchev <r.genchew@gmail.com>
2020-07-05 23:16:48 +0200

    Install and configure gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,15 @@ module.exports = {
         display: 'standalone',
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
